eeb8c034ea6a7b4009729976fd4db91327f59a48
Fatuma <sfatex@gmail.com>
2021-08-17 11:54:25 -0400

    initial commit

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if ((letters.length === 0) || typeof (word) === undefined) {
+        return false;
     }
-}
 
-module.exports = wordSearch
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (const horLetter of horizontalJoin) {
+        if (horLetter.includes(word)) {
+            return true;
+        }
+    }
+    //interchange row and column 
+    let toVerticaArr = [];
+    for (let column = 0; column < letters[0].length; column++) {
+        toVerticaArr.push([]);
+        for (let row = 0; row < letters.length; row++) {
+            toVerticaArr[column].push(letters[row][column]);
+        }
+    }
+
+    const vericalJoin = toVerticaArr.map(hs => hs.join(''));
+    for (const verLetter of vericalJoin) {
+        if (verLetter.includes(word)) {
+            return true;
+        }
+    }
+    return false;
+};
+
+module.exports = wordSearch;
+
